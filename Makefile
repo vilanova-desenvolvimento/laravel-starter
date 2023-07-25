bf98c0daec9ac6b178f855e1e48f37426b935d2b
inf-sid <sidnei.simeao@vilanova.com.br>
2023-07-25 13:23:59 -0300

    Melhorias no arquivo entrypoint, e inclusão do arquivo Makefile

--- Makefile
+++ Makefile
@@ -1,23 +1,31 @@
-# Leitura das variáveis do arquivo .env
-include .env
-export
+# Verifica se o arquivo .env existe antes de incluí-lo
+ifneq ($(wildcard .env),)
+    include .env
+    export
+endif
 
-# Converter a variável APP_NAME para minúsculas e concatenar com '-app'
-APP_CONTAINER = $(addsuffix -app,$(shell echo $(APP_NAME) | tr '[:upper:]' '[:lower:]'))
+# Converter a variável APP_NAME para minúsculas, substituir espaços em branco por hífens e remover caracteres não alfanuméricos
+APP_CONTAINER = $(addsuffix -app, $(shell echo $(APP_NAME) | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | sed -E 's/[^a-zA-Z0-9-]//g'))
 
-.PHONY: help install
+# Ler o conteúdo da variável APP_ENV
+APP_ENV_CONTENT := $(APP_ENV)
+
+.PHONY: help install uninstall
 
 help: ## Print help.
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n\nTargets:\n"} /^[a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-10s\033[0m %s\n", $$1, $$2 }' $(MAKEFILE_LIST)
 
-install: ## Inicia a aplicação.
-	@docker compose up -d && \
-		docker compose exec $(APP_CONTAINER) composer install && \
-		docker compose exec $(APP_CONTAINER) php artisan key:generate
-	
+install: ## Cria os arquivos de configuração de aplicação, sobe os containers e instala as suas dependencias
+	@sh ./entrypoint.sh
 
+## Impede que estes comandos sejam executados por em produção
+ifeq ($(strip $(APP_ENV_CONTENT)),local)
 
+uninstall: ## Derruba os container da aplicação e limpa arquivos relacionados.	
+	@rm -rf docker-compose
+	@rm -f composer.lock
+	@rm -f .env
+	@docker compose down --remove-orphans
+	@rm -f docker-compose.yml
 
-
-
-
+endif
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,54 +4,62 @@
 env_example=".env.example"
 env_file=".env"
 
-# Copiar o arquivo .env.example para .env, se o arquivo .env não existir
-if [ ! -f "$env_file" ]; then
-    cp "$env_example" "$env_file"
-    echo -e "\e[32mArquivo $env_file copiado do $env_example.\e[0m"
-else
-    echo -e "\e[33mO arquivo $env_file já existe. Não será copiado novamente. Encerrando o script. \e[0m"
-    exit 1
-fi
+# Obter o valor da variável APP_IMAGE do arquivo .env
+app_image=$(grep -E "^APP_IMAGE=" "$env_example" | cut -d '=' -f2 | sed -e 's/^"//' -e 's/"$//')
+# Obter o valor da variável APP_PORT do arquivo .env e remover quaisquer caracteres não numéricos
+app_port=$(grep -E "^APP_PORT=" "$env_example" | cut -d '=' -f2 | tr -cd '[:digit:]')
 
 # Verificar se a variável APP_NAME existe no arquivo .env
-app_name=$(grep -E "^APP_NAME=" "$env_file" | cut -d '=' -f2 | sed -e 's/^"//' -e 's/"$//')
-# Obter o valor da variável APP_IMAGE do arquivo .env
-app_image=$(grep -E "^APP_IMAGE=" "$env_file" | cut -d '=' -f2 | sed -e 's/^"//' -e 's/"$//')
-# Obter o valor da variável APP_PORT do arquivo .env e remover quaisquer caracteres não numéricos
-app_port=$(grep -E "^APP_PORT=" "$env_file" | cut -d '=' -f2 | tr -cd '[:digit:]')
-
+app_name=$(grep -E "^APP_NAME=" "$env_example" | cut -d '=' -f2 | sed -e 's/^"//' -e 's/"$//')
 # Converter o valor da variável APP_NAME para minúsculas
 app_name=$(echo "$app_name" | tr '[:upper:]' '[:lower:]')
-
-# Substituir o valor da variável REDIS_HOST no arquivo .env pelo valor ${app_name}-redis
-sed -i "s/^REDIS_HOST=.*/REDIS_HOST=${app_name}-redis/" "$env_file"
-
-echo -e "\e[32mValor da variável REDIS_HOST substituído por ${app_name}-redis no arquivo $env_file.\e[0m"
+# Substituir espaços em branco por hífens e remover caracteres não alfanuméricos
+app_name=$(echo "$app_name" | tr ' ' '-' | sed -E 's/[^a-zA-Z0-9-]//g')
 
 # Verificar se a variável APP_NAME existe e tem pelo menos 3 caracteres
 if [ -z "$app_name" ] || [ ${#app_name} -lt 3 ]; then
-    echo -e "\e[31mA variável APP_NAME não está definida no arquivo .env.example ou possui menos de 3 caracteres. Encerrando o script.\e[0m"
+    echo "\e[31mA variável APP_NAME não está definida no arquivo .env.example ou possui menos de 3 caracteres. Encerrando o script.\e[0m"
+fi
+
+# Verificar se existe um container com o mesmo nome da aplicação
+if docker ps --format "{{.Names}}" | grep -q "^${app_name}-app"; then
+    echo "\e[31mJá existe um container com o nome '${app_name}-app' em execução. \nSe estiver tentando subir uma nova aplicaçação, altere o valor de APP_NAME. \nEncerrando o script.\e[0m"
+    exit 1
+fi
+
+# Copiar o arquivo .env.example para .env, se o arquivo .env não existir
+if [ ! -f "$env_file" ]; then
+    cp "$env_example" "$env_file"
+    echo "\e[32mArquivo $env_file copiado do $env_example.\e[0m"
+
+    # Substituir o valor da variável REDIS_HOST no arquivo .env pelo valor ${app_name}-redis
+    sed -i "s/^REDIS_HOST=.*/REDIS_HOST=${app_name}-redis/" "$env_file"
+
+    echo "\e[32mValor da variável REDIS_HOST substituído por ${app_name}-redis no arquivo $env_file.\e[0m"
+else
+    echo "\e[33mO arquivo $env_file já existe. Não será copiado novamente. Encerrando o script. \e[0m"
+    exit 1
 fi
 
 # Verificar se o arquivo docker-compose.yml já existe
 docker_compose_file="docker-compose.yml"
 if [ -f "$docker_compose_file" ]; then
-    echo -e "\e[31mO arquivo $docker_compose_file já existe. Encerrando o script.\e[0m"
+    echo "\e[31mO arquivo $docker_compose_file já existe. Encerrando o script.\e[0m"
 fi
 
 # Verificar se o diretório docker-compose/nginx existe, se não, criá-lo
 nginx_dir="docker-compose/nginx/"
 if [ ! -d "$nginx_dir" ]; then
     mkdir -p "$nginx_dir"
-    echo -e "\e[32mDiretório $nginx_dir criado com sucesso.\e[0m"
+    echo "\e[32mDiretório $nginx_dir criado com sucesso.\e[0m"
 else
-    echo -e "\e[33mO diretório $nginx_dir já existe. Não será criado novamente.\e[0m"
+    echo "\e[33mO diretório $nginx_dir já existe. Não será criado novamente.\e[0m"
 fi
 
 # Verificar se o arquivo nginx.conf já existe
 nginx_conf="docker-compose/nginx/nginx.conf"
 if [ -f "$nginx_conf" ]; then
-    echo -e "\e[31mO arquivo $nginx_conf já existe. Encerrando o script.\e[0m"
+    echo "\e[31mO arquivo $nginx_conf já existe. Encerrando o script.\e[0m"
     exit 1
 fi
 
@@ -80,7 +88,7 @@ echo "server {
     access_log /var/log/nginx/access.log;
 }" > "$nginx_conf"
 
-echo -e "\e[32mArquivo $nginx_conf criado com sucesso!\e[0m"
+echo "\e[32mArquivo $nginx_conf criado com sucesso!\e[0m"
 
 # Criar o arquivo docker-compose.yml
 echo "version: \"3.8\"
@@ -141,9 +149,21 @@ networks:
   ${app_name}:
     name: ${app_name}" > "$docker_compose_file"
 
-echo -e "\e[32mArquivo $docker_compose_file criado com sucesso!\e[0m"
+echo "\e[32mArquivo $docker_compose_file criado com sucesso!\e[0m"
 
-# Comando make install
-make install
+echo "\e[32mInstalando as dependencias da aplicação \e[0m"
 
-echo -e "\e[32mAplicação acessivel em: http://localhost:${app_port} \e[0m"
+# Instala as dependencias da aplicação
+docker compose up -d && \
+		docker compose exec ${app_name}-app composer install --quiet && \
+		docker compose exec ${app_name}-app php artisan key:generate
+
+echo "\e[37m\e[44mAplicação acessível em: http://localhost:${app_port} \e[0m"
+
+
+
+
+
+
+
+
